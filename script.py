35320801ea94432af5adcb4254169b5d210d9783
nightmngr <gordan4444@gmail.com>
2018-11-28 23:52:59 +0100

    CodeCademy -45%

--- script.py
+++ script.py
@@ -112,3 +112,28 @@ inventory['backpack'].remove('dagger')
 inventory['gold'] += 50
 
 # / arrays
+#
+#
+#
+#
+# / lists
+
+shopping_list = ["banana", "orange", "apple"]
+stock = { "banana": 6, "apple": 0, "orange": 32, "pear": 15 }
+prices = { "banana": 4, "apple": 2, "orange": 1.5, "pear": 3 }
+
+# Write your code below!
+def compute_bill(food):
+  total = 0
+  for item in food:
+    if stock[item] > 0:
+      total = total + prices[item]
+      stock[item] -= 1
+  print total
+  return total
+
+# smetka
+compute_bill(shopping_list)
+print stock
+
+# / lists
